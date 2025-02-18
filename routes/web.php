3a694846c991b9296d33c2137165d601637eb529
halilibrahimsaltas <h.ibrahimsaltas@mail.com>
2025-02-18 16:20:34 +0300

    Cache,optimize

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\SupportFormController;
 use App\Http\Controllers\UserController;
+
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
 Route::get('/about', [HomeController::class, 'about'])->name('about');
@@ -17,12 +18,17 @@ Route::get("/user/{id}", [HomeController::class, 'user'])->name('user');
 
 Route::match(["get", "post"], "support-form", [SupportFormController::class, "supportForm"])->name("support-form");
 
-Route::patch("user/{id}/guncelle", [ContactController::class, "update"])->name("user.update");
+Route::patch("user/{id}/guncelle", [ContactController::class, "update"])->name("user.update.patch");
 
-Route::put("user/{id}/guncelle", [ContactController::class, "update"])->name("user.update");
+Route::put("user/{id}/guncelle", [ContactController::class, "update"])->name("user.update.put");
 
 Route::resource("/api/articles", "ArticleController");
 
 Route::apiResource("/api/articles", "Api/ArticleController");
 
 Route::get("/user/{id}", [UserController::class, "show"])->name("user.show");
+
+Route::prefix("admin")->group(function () {
+    Route::get("/edit-article/{id}", "ArticleController@edit")->name("admin.edit-article");
+    Route::delete("/delete-article/{id}", "ArticleController@destroy")->name("admin.delete-article");
+});
